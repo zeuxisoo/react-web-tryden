12ddb0682f3a9f7c23e5c2a8ca13b864f14425ee
Zeuxis Lo <seekstudio@gmail.com>
2019-09-02 17:01:01 +0800

    add the editor config and formatted all related files

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      It is worked!
-    </div>
-  );
+    return (
+        <div className="App">
+            It is worked!
+        </div>
+    );
 }
 
 export default App;
