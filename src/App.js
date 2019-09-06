856fbd22a8141db67d7925d71fb3cadaa3ce5e37
Zeuxis Lo <seekstudio@gmail.com>
2019-09-06 14:07:09 +0800

    move all Board related components into single Board component file

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import { Drawer, DrawerBackdrop, DrawerPaper } from './components/Drawer';
 import Wrapper from './components/Wrapper';
 import Navbar from './components/Navbar';
 import Board from './components/Board';
-import { Topics, Topic } from './components/Topics';
-import { Threads, Thread } from './components/Threads';
+
 import theme from './theme';
 import './App.css';
 
@@ -35,18 +34,7 @@ function App() {
                 </Drawer>
                 <Wrapper>
                     <Navbar />
-                    <Board>
-                        <Topics>
-                            {[...Array(10)].map((x, i) =>
-                                <Topic key={i}>Topic {i+1}</Topic>
-                            )}
-                        </Topics>
-                        <Threads>
-                            {[...Array(10)].map((x, i) =>
-                                <Thread key={i}>Thread {i+1}</Thread>
-                            )}
-                        </Threads>
-                    </Board>
+                    <Board />
                 </Wrapper>
             </div>
         </ThemeProvider>
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,27 @@
+import React from 'react';
 import styled from 'styled-components';
 
-const Board = styled.div`
+import { Topics, Topic } from './Topics';
+import { Threads, Thread } from './Threads';
+
+function Board() {
+    return (
+        <BoardContainer>
+            <Topics>
+                {[...Array(10)].map((x, i) =>
+                    <Topic key={i}>Topic {i+1}</Topic>
+                )}
+            </Topics>
+            <Threads>
+                {[...Array(10)].map((x, i) =>
+                    <Thread key={i}>Thread {i+1}</Thread>
+                )}
+            </Threads>
+        </BoardContainer>
+    );
+}
+
+const BoardContainer = styled.div`
     display: grid;
     grid-template-columns: 25% 1fr;
     grid-template-rows: 1fr;
