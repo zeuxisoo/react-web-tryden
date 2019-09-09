fd7dbcc58b525708dc61a948a5847f47c1f9094b
Zeuxis Lo <seekstudio@gmail.com>
2019-09-09 18:46:23 +0800

    add close animation for Drawer component

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
 
 import MainContext from '../hook/contexts/main';
@@ -7,17 +7,24 @@ import types from '../hook/types';
 function Drawer() {
     /* eslint-disable no-unused-vars */
     const [mainState, dispatch] = useContext(MainContext);
+    const [isClosing, setIsClosing] = useState(false);
 
     function handleDrawerClose() {
-        dispatch({
-            type: types.SET_DRAWER_CLOSE
-        });
+        setIsClosing(true);
+
+        setTimeout(() => {
+            dispatch({
+                type: types.SET_DRAWER_CLOSE
+            });
+
+            setIsClosing(false);
+        }, 300);
     }
 
     return (
         <DrawerContainer isOpen={mainState.isDrawerOpen}>
-            <DrawerBackdrop isOpen={mainState.isDrawerOpen} onClick={handleDrawerClose} />
-            <DrawerPaper isOpen={mainState.isDrawerOpen}>
+            <DrawerBackdrop isOpen={mainState.isDrawerOpen} isClosing={isClosing} onClick={handleDrawerClose} />
+            <DrawerPaper isOpen={mainState.isDrawerOpen} isClosing={isClosing}>
                 Hello World
             </DrawerPaper>
         </DrawerContainer>
@@ -43,9 +50,14 @@ const DrawerBackdrop = styled.div`
     opacity: 1;
     background-color: ${props => props.theme.drawerBackdropColor};
 
-    //
+    // Shared animation
     transition: opacity 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+
+    // Animation (isOpen): fade in
     opacity: ${props => props.isOpen === true ? 1 : 0};
+
+    // Animation (isClosing): fade out
+    opacity: ${props => props.isClosing === true ? 0 : 1};
 `;
 
 const DrawerPaper = styled.div`
@@ -58,11 +70,16 @@ const DrawerPaper = styled.div`
     background-color: ${props => props.theme.drawerPaperBackgroundColor};
     opacity: 0.9;
 
-    // Slide left to right from -300px to 0px
-    left: ${props => props.isOpen === true ? '-300px' : ''};
-
+    // Shared animation
     transition: transform 300ms ease-in-out;
+
+    // Animation (isOpen): slide left to right from -300px to 0px
+    left: ${props => props.isOpen === true ? '-300px' : ''};
     transform: ${props => props.isOpen === true ? 'translate(300px, 0px)' : ''};
+
+    // Animation (isClosing): slide right to left from 0px to -300px
+    left: ${props => props.isClosing === true ? '-300px' : ''};
+    transform: ${props => props.isClosing === true ? 'translate(0px, 0px)' : ''};
 `;
 
 export default Drawer;
